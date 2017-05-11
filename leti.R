624aa09d76497eda6ffb6f1ca7aa4914b456e8a3
Manca Cör <manca.cor@student.fmf.uni-lj.si>
2017-05-11 14:01:49 +0200

    Leti

--- leti.R
+++ leti.R
@@ -1,17 +1,16 @@
 library(XML)
-
-
-url <- "https://www.ryanair.com/gb/en/cheap-flights/?from=TSF&out-from-date=2017-05-11&out-to-date=2017-08-11&in-from-date=2017-05-11&in-to-date=2017-08-11&budget=150&trip-type-category=GLF"
-urldata <- getURL(url)
-# Parse the XML file
-
-
-data <- readHTMLTable(urldata,stringsAsFactors = FALSE)
-
+library(gsubfn)
 
 url<-"http://www.edreams.com/offers/flights/airline/W6/wizz-air/"
 urldata <- getURL(url)
 data <- readHTMLTable(urldata,stringsAsFactors = FALSE)
+z <- substr(a[1,1],10,10)
+A<-c()
+for (i in data){
+  A<-c(A,i$V1,i$V2)
+}
 
+mat1 <- A %>% na.omit() %>% strapplyc(paste0("^(.*) ", z, " (.*)€([0-9]+)")) %>% data.frame() %>% t()
 
+A<-c(a$V1,a$V2)
 a<-data[[1]]
